90884645d1a2bf0cd47e2e17aaf933a0530dbda5
Max Thom Stahl <max@villainousindustri.es>
2011-12-13 00:09:52 -0600

    Made a little clearer that the update_attributes function isn't yet atomic.

--- spec/models/updates_spec.rb
+++ spec/models/updates_spec.rb
@@ -16,10 +16,6 @@ describe 'queries', :queries => true do
   
   ###
   
-  it 'should openly refuse to set one field that does not exist' do
-    (->{@bike.update_attribute :this_field_does_not_exist, "This is never gonna get stored."}).should raise_error
-  end
-  
   it 'should be able to update one field atomically' do
     @bike.model.should == "CB400"
     @bike.update_attribute :model, "CB450SC"
@@ -40,6 +36,21 @@ describe 'queries', :queries => true do
     @bike.year.should == "1996"
     @bike.reload.model.should == "CB750"
     @bike.reload.year.should == "1996"
+  end
+
+  it 'should openly refuse to set fields that do not exist' do
+    (->{
+      @bike.update_attribute :this_field_does_not_exist, "Lorem ipsum dolor sit amet."
+    }).should raise_error
+    (->{
+      # TODO Once this operation is made atomic, this test needs to be updated to also
+      # include keys that do exist, in various orders. Even if real fields are included,
+      # no fields should be updated if any part of this query would fail. To emulate this
+      # behaviour in the meantime, possibly check all keys of attributes hash to see if 
+      # they exist before performing the queries or synchronize during.
+      @bike.update_attributes this_field_does_not_exist: "Ut enim ad minim veniam.",
+                              neither_does_this_one:     "Duis aute irure dolor in."
+    }).should raise_error
   end
   
   ###
